4457b5d512fdd32a3b1c3cc6927e04f3086178d3
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-08-14 21:17:20 +0530

    crd commit

--- src/app/components/personal_detail_card.js
+++ src/app/components/personal_detail_card.js
@@ -3,14 +3,15 @@ const PersonalDetailCard = () => {
   let userDetails;
   try{
   userDetails = JSON.parse(localStorage.getItem('userDetails') || "");
+  // console.log("userDetails",userDetails.name,)
 } catch (error) {}
   return (
     <div className="bg-amber-800 shadow-md rounded-lg overflow-hidden h-96 ">
       <div className="p-4">
         <h2 className="text-3xl font-semibold mb-2 text-white">Personal Details</h2>
-        <h3 className="text-xl font-semibold mb-2 text-white">Name - {userDetails.name}</h3>
-        <h3 className="text-xl font-semibold mb-2 text-white">Jobrole - {userDetails.jobrole}</h3>
-        <h3 className="text-xl font-semibold mb-2 text-white">Company - {userDetails.company}</h3>
+        <h3 className="text-xl font-semibold mb-2 text-white">Name - Admin</h3>
+        <h3 className="text-xl font-semibold mb-2 text-white">Jobrole - Admin</h3>
+        <h3 className="text-xl font-semibold mb-2 text-white">Company - SDS</h3>
       </div>
     </div>
   );
