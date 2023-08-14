72c8ef214e41cd129eea4801ce7f6e2bb56dd764
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-08-14 19:47:06 +0530

    save alert commit

--- src/app/components/save_alert.js
+++ src/app/components/save_alert.js
@@ -3,7 +3,7 @@ const SaveAlert = ({description , setShowAlert}) => {
 
     return (
         <div
-        className="mb-4 rounded-lg bg-green-500 px-6 py-5 text-base text-red-100"
+        className="mb-4 rounded-lg bg-green-500 px-6 py-5 text-base text-red-100 absolute bottom-10 right-50"
         role="alert"
       >
         {description}
